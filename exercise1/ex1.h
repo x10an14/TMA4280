8afaab073d8bd6a078074a38891db82c886cde7f
x10an14 <chrischa@stud.ntnu.no>
2014-01-14 19:51:49 +0100

    initial commit of ex1

---
+++ exercise1/ex1.h
@@ -0,0 +1,10 @@
+#ifndef EXERCISE_1_
+#define EXERCISE_1_
+
+int const size = 3;
+
+extern const double tmpX[size*sizeof(double)];
+extern const double tmpY[size*size*sizeof(double)];
+extern const double tmpA[size*size*sizeof(double)];
+
+#endif
