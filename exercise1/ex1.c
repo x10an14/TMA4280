27acfe7029b12d6becd3e8d96e4a276710f71eb1
x10an14 <chrischa@stud.ntnu.no>
2014-01-14 20:27:34 +0100

    Removed potential bug and cleaned up a little.

--- exercise1/ex1.c
+++ exercise1/ex1.c
@@ -1,19 +1,20 @@
 #include <stdio.h>	//printf
 #include <stdlib.h>	//malloc
 #include <string.h>	//memcpy
-#include "ex1.h"
+#include "ex1.h" //because of the hardcoded values (temp<x/A> variables)
 
 int main(int argc, char const *argv[]){
 	const double tmpX[] = {1.0, 1.0, 1.0};
 	const double tmpA[] = {0.3, 0.4, 0.3, 0.7, 0.1, 0.2, 0.5, 0.5, 0.0};
 
-	double *y = (double*) malloc(size*sizeof(double));
-	double **A = (double**) malloc(size*sizeof(double*));
-	A[0] = malloc(size*size*sizeof(double));
+	double *y = (double*) calloc(size*sizeof(double));
 	double *x = (double*) malloc(size*sizeof(double));
 
+	double **A = (double**) malloc(size*sizeof(double*));
+	A[0] = malloc(size*size*sizeof(double));
+
 	for (int i = 1; i < size; ++i){
-		A[i] = A[i-1] + size; //Make each pointer beyond i > 0 point at the correct place in the malloced memory.
+		A[i] = A[i-1] + size; //Make each pointer beyond i > 0 point at the correct place in the malloc'd memory.
 	}
 
 	memcpy(x, tmpX, size*sizeof(double));
